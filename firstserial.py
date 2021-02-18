648f267fd36497366f0b1f9420f5acdd05114c45
Pritam Pun <46627965+Pritampun@users.noreply.github.com>
2021-02-18 07:51:17 +0545

    Update firstserial.py

--- firstserial.py
+++ firstserial.py
@@ -1,18 +1,20 @@
-import serial                                                # for serial communication
-import time                                                  #to able to use delay(i.e. time .sleep function )
 
+import serial
+import time
+x=input('Enter size:'+'\n'+'a.Small'+'\t\t'+'b.Big'+'\t\t')
+y=input('Enter color:'+'\n'+'a.Blue'+'\t\t'+'b.Red'+'\t\t')
 ser = serial.Serial('COM3', baudrate = 9600, timeout = 1)
-time.sleep(3)                                                #jsut a delay of 3 sec.
-userInput = input('Do you want to give datas to arduino?''\n''a.YES''\t''b.NO''\t''\t''\t') 
-if userInput =='YES' or userInput =='yes':                   #Give data to arduino only if user type YES
-    Num = input('Enter how many LED?''\t''\t')               #enter how many leds or sensors are attached
-    Num = int (Num)                                          #input values gives str so to convert str to int
-    dataList = [0]*Num                                       #just creat list with size of led attached
-    for i in range (0,Num):
-         dataList[i]=input('Enter which one to on?''\n''1.a''\t''2.b''\t''3.c''\t''\t')
-         ser.write(dataList[i].encode())
-         time.sleep(1)                 
-elif userInput =='NO' or userInput =='no':
-     print('Ok no LED will blink')
-else:
-    print(' error!!!   '+ userInput +'   is not recognize!!!')
+time.sleep(3)
+data=['1','2','3','4']
+if ((x=='Small'or x=='small' or x=='SMALL') and (y=='Blue' or y=='blue' or y=='BLUE')):
+     ser.write(data[0].encode())
+     time.sleep(1)
+elif (x=='Small'or x=='small' or x=='SMALL') and (y=='Red'or y=='red' or y=='RED'):
+     ser.write(data[1].encode())
+     time.sleep(1)
+elif (x=='Big'or x=='big' or x=='BIG') and (y=='Blue' or y=='blue' or y=='BLUE'):
+     ser.write(data[2].encode())
+     time.sleep(1)
+elif (x=='Big'or x=='big' or x=='BIG') and (y=='Red' or y=='red' or y=='RED'):
+     ser.write(data[3].encode())
+     time.sleep(1) 
