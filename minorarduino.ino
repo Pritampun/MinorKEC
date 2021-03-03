1da8c4189ae46df80647e144dd16fd9bf2af847c
Pritam Pun <46627965+Pritampun@users.noreply.github.com>
2021-03-03 12:21:01 +0545

    Update minorarduino.ino

--- minorarduino.ino
+++ minorarduino.ino
@@ -1,69 +1,265 @@
 #include <Servo.h>
 
-Servo s1,s2,s3,s4,s5; 
-int pos = 0,data;
+Servo s1,s2,s3,s4,s5,s6; 
+int ipos1,ipos2,ipos3,ipos4,ipos5,ipos6,data;
+int pos=0,ipos=0;
 void setup() {
   pinMode(10,OUTPUT);
-  s1.attach(3);
-  s2.attach(4);
-  s3.attach(5);
-  s4.attach(6);
- s5.attach(7);
+  s1.attach(2);//claw
+  s2.attach(3);//plam
+  s3.attach(4);//ankle
+  s4.attach(5);//elbow
+  s5.attach(6);//shoulder
+  s6.attach(7);//base
   Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available()>0)  
   { 
+    //take the object from its location
+      for(pos=90;pos>=0;pos--)
+     {
+      s6.write(pos);
+      delay(5);
+     }
+     delay(1000);
+     for(pos=90;pos>=40;pos--)
+     {
+      s5.write(pos);
+      delay(10);
+     }
+     delay(1000);
+     for(pos=90;pos<=110;pos++)
+     { 
+      s4.write(pos);
+      delay(10);
+     }
+     delay(1000);
+     for(pos=90;pos<=180;pos++)
+     {
+      s1.write(pos);
+      delay(10);
+     }
+     delay(1000);
+     for(pos=90;pos>=40;pos--)
+     {
+      s2.write(pos);
+      delay(10);
+     }
+     delay(1000);
+     
+     //take object to its destination location
+     for(pos=180;pos>=0;pos--)
+     {
+      s1.write(pos);
+      delay(10);
+     }
+     delay(1000);
+     for(pos=40;pos<=100;pos++)
+     {
+      s2.write(pos);
+      delay(10);
+     }
+     delay(1000);
+     for(pos=110;pos>=90;pos--)
+     {
+      s4.write(pos);
+      delay(10);
+     }
+     for(pos=40;pos<=90;pos++)
+     {
+      s5.write(pos);
+      delay(10);
+     }
     data=Serial.read();      
     if(data=='1')
     {
-      s1.write(0);
-      s2.write(0);
-      s3.write(0);
-      s4.write(0);
-      s5.write(0);
-     delay(5000);
-      s1.write(180);
-      s2.write(180);
-      s3.write(180);
-      s4.write(180);
-      s5.write(180);
+      
+     //to place object to their respective places
+     for(pos=0;pos<=180;pos++)
+     {
+      s6.write(pos);
+      delay(5);
+      ipos6=pos;
+     }
+     for(pos=90;pos>=30;pos--)
+     { 
+      s5.write(pos);
+      delay(10);
+      ipos5=pos;
+     }
+     delay(1000);
+     for(pos=90;pos<=120;pos++)
+     {
+      s4.write(pos);
+      delay(10);
+      ipos4=pos;
+     }
+     delay(1000);
+     for(pos=100;pos>=40;pos--)
+     {
+      s2.write(pos);
+      delay(10);
+      ipos2=pos;
+     }
+     delay(1000);
+     for(pos=0;pos<=180;pos++)
+     {
+      s1.write(pos);
+      delay(10);
+      ipos1=pos;
+     }
+     delay(1000);
     }
 
     else if(data=='2')
     {
-      for(pos=0;pos<=100;pos++)
-      {
-        s1.write(pos);
-        s2.write(pos);
-        delay(10); 
-      }
+      //to place object to their respective places
+      for(pos=0;pos<=180;pos++)
+     {
+      s6.write(pos);
+      delay(5);
+      ipos6=pos;
+     }
+     for(pos=90;pos>=40;pos--)
+     { 
+      s5.write(pos);
+      delay(10);
+      ipos5=pos;
+     }
+     delay(1000);
+     for(pos=90;pos<=130;pos++)
+     {
+      s4.write(pos);
+      delay(10);
+      ipos4=pos;
+     }
+     delay(1000);
+     for(pos=100;pos>=40;pos--)
+     {
+      s2.write(pos);
+      delay(10);
+      ipos2=pos;
+     }
+     delay(1000);
+     for(pos=0;pos<=180;pos++)
+     {
+      s1.write(pos);
+      delay(10);
+      ipos1=pos;
+     }
     }
+  
    else if(data=='3')
     {
-      for(pos=0;pos<=80;pos++)
-      {
-        s1.write(pos);
-        s2.write(pos);
-        delay(10);
-      }
+      //to place object to their respective places
+      for(pos=0;pos<=150;pos++)
+     {
+      s6.write(pos);
+      delay(5);
+      ipos6=pos;
+     }
+     for(pos=90;pos>=30;pos--)
+     { 
+      s5.write(pos);
+      delay(10);
+      ipos5=pos;
+     }
+     delay(1000);
+     for(pos=90;pos<=120;pos++)
+     {
+      s4.write(pos);
+      delay(10);
+      ipos4=pos;
+     }
+     delay(1000);
+     for(pos=100;pos>=40;pos--)
+     {
+      s2.write(pos);
+      delay(10);
+      ipos2=pos;
+     }
+     delay(1000);
+     for(pos=0;pos<=180;pos++)
+     {
+      s1.write(pos);
+      delay(10);
+      ipos1=pos;
+     }
     }
    else if(data=='4')
     {
-      for(pos=0;pos<=50;pos++)
-      {
-        s1.write(pos);
-        s2.write(pos);
-        delay(10);
-      }
+      //to place object to their respective places
+      for(pos=0;pos<=150;pos++)
+     {
+      s6.write(pos);
+      delay(5);
+      ipos6=pos;
+     }
+     for(pos=90;pos>=40;pos--)
+     { 
+      s5.write(pos);
+      delay(10);
+      ipos5=pos;
+     }
+     delay(1000);
+     for(pos=90;pos<=130;pos++)
+     {
+      s4.write(pos);
+      delay(10);
+      ipos4=pos;
+     }
+     delay(1000);
+     for(pos=100;pos>=40;pos--)
+     {
+      s2.write(pos);
+      delay(10);
+      ipos2=pos;
+     }
+     delay(1000);
+     for(pos=0;pos<=180;pos++)
+     {
+      s1.write(pos);
+      delay(10);
+      ipos1=pos;
+     }
     }
-    else
-    {
-      digitalWrite(10,HIGH);
-      delay(1000);
-      digitalWrite(10,LOW);
-    }
+    //to go to initial position which is 90 degree(IDEAL position for all 180 degree servos)
 
+    for(ipos=ipos2;ipos<=130;ipos++)
+    {
+      s2.write(ipos);
+      delay(10);
+    }
+    for(ipos=ipos1;ipos>=90;ipos--)
+     {
+      s1.write(ipos);
+      delay(10);
+    }
+    delay(1000);
+    for(ipos=ipos4;ipos>=90;ipos--)
+     {
+      s4.write(ipos);
+      delay(10);
+    }
+    delay(1000);
+    for(ipos=ipos5;ipos<=90;ipos++)
+     {
+      s5.write(ipos);
+      delay(10);
+    }
+    delay(1000);
+    for(ipos=ipos6;ipos>=90;ipos--)
+     {
+      s6.write(ipos);
+      delay(5);
+    }
+    delay(1000);
+    for(ipos=ipos2;ipos<=90;ipos++)
+     {
+      s2.write(ipos);
+      delay(10);
+    }
   }
 }
