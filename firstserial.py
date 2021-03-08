40f690dfd782f62cbfe32ddfe2f89cd85d07e6cd
Pritam Pun <46627965+Pritampun@users.noreply.github.com>
2021-03-08 13:03:30 +0545

    Update firstserial.py

--- firstserial.py
+++ firstserial.py
@@ -1,20 +1,21 @@
 
 import serial
 import time
-x=input('Enter size:'+'\n'+'a.Small'+'\t\t'+'b.Big'+'\t\t')
-y=input('Enter color:'+'\n'+'a.Blue'+'\t\t'+'b.Red'+'\t\t')
-ser = serial.Serial('COM3', baudrate = 9600, timeout = 1)
+
+x = (input('Enter size:'+'\n'+'a.Small'+'\t\t'+'b.Big'+'\t\t')).lower()
+y = (input('Enter color:'+'\n'+'a.green'+'\t\t'+'b.Red'+'\t\t')).lower()
+ser = serial.Serial('COM5', 9600, timeout=1)
 time.sleep(3)
-data=['1','2','3','4']
-if ((x=='Small'or x=='small' or x=='SMALL') and (y=='Blue' or y=='blue' or y=='BLUE')):
-     ser.write(data[0].encode())
-     time.sleep(1)
-elif (x=='Small'or x=='small' or x=='SMALL') and (y=='Red'or y=='red' or y=='RED'):
-     ser.write(data[1].encode())
-     time.sleep(1)
-elif (x=='Big'or x=='big' or x=='BIG') and (y=='Blue' or y=='blue' or y=='BLUE'):
-     ser.write(data[2].encode())
-     time.sleep(1)
-elif (x=='Big'or x=='big' or x=='BIG') and (y=='Red' or y=='red' or y=='RED'):
-     ser.write(data[3].encode())
-     time.sleep(1) 
+if(x == "small" and y == "blue"):
+    ser.write(b'A')
+    time.sleep(1)
+elif(x == "small" and y == "red"):
+    ser.write(b'B')
+    time.sleep(1)
+elif(x =="big" and y=="blue"):
+    ser.write(b'C')
+    time.sleep(1)
+elif(x == "big" and y == "red"):
+    ser.write(b'D')
+    time.sleep(1)
+
